6b7aee49bf5b82f27061f7a1b38293f9c1f81722
Mote <mark.l.mote@jpl.nasa.gov>
2019-06-26 17:51:06 -0700

    add readme and function dsecriptions

--- coverage_optimizer/get_node_neighbors.m
+++ coverage_optimizer/get_node_neighbors.m
@@ -1,8 +1,43 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2019 by California Institute of Technology.  ALL RIGHTS RESERVED. %
+% United  States  Government  sponsorship  acknowledged.   Any commercial use %
+% must   be  negotiated  with  the  Office  of  Technology  Transfer  at  the %
+% California Institute of Technology.                                         %
+%                                                                             %
+% This software may be subject to  U.S. export control laws  and regulations. %
+% By accepting this document,  the user agrees to comply  with all applicable %
+% U.S. export laws and regulations.  User  has the responsibility  to  obtain %
+% export  licenses,  or  other  export  authority  as may be required  before %
+% exporting  such  information  to  foreign  countries or providing access to %
+% foreign persons.                                                            %
+%                                                                             %
+% This  software  is a copy  and  may not be current.  The latest  version is %
+% maintained by and may be obtained from the Mobility  and  Robotics  Sytstem %
+% Section (347) at the Jet  Propulsion  Laboratory.   Suggestions and patches %
+% are welcome and should be sent to the software's maintainer.                %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function unique_new_neighbors = get_node_neighbors(vertices , neighbor_set)
+%GET_NODE_NEIGHBORS returns the set all neighboring vertices to a given set
+%of vertices. Removes repreated elements. 
+%   Syntax: unique_new_neighbors = get_node_neighbors(vertices , neighbor_set)
+%   
+%   Inputs: 
+%    - verticies [N_VERTICES x 1]: The indicies for a set of verticies  
+%    - neighbor_set [N_VERTICES x 1]: A cell containing the neighbors (i.e.
+%    connected verticies) for each vertex. 
+%       e.g. neighbor_set{i} will contain all verticies sharing a face with
+%       vertex i 
+% 
+%   Outputs: 
+%    - unique_new_neighbors: A vector of all neighboring vertices to the
+%    given input set, with repeated elements removed.
 
 sort = false; 
 
 new_neighbors = []; % neighbors of first vertex
+
 for i = 1:length(vertices)
     new_neighbors = [new_neighbors, neighbor_set{vertices(i)}];
 end
@@ -11,6 +46,7 @@ if sort==true
 else
     unique_new_neighbors = unique(new_neighbors);
 end
+
 end
 
 
--- coverage_optimizer/get_voronoi_data.m
+++ coverage_optimizer/get_voronoi_data.m
@@ -1,4 +1,50 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2019 by California Institute of Technology.  ALL RIGHTS RESERVED. %
+% United  States  Government  sponsorship  acknowledged.   Any commercial use %
+% must   be  negotiated  with  the  Office  of  Technology  Transfer  at  the %
+% California Institute of Technology.                                         %
+%                                                                             %
+% This software may be subject to  U.S. export control laws  and regulations. %
+% By accepting this document,  the user agrees to comply  with all applicable %
+% U.S. export laws and regulations.  User  has the responsibility  to  obtain %
+% export  licenses,  or  other  export  authority  as may be required  before %
+% exporting  such  information  to  foreign  countries or providing access to %
+% foreign persons.                                                            %
+%                                                                             %
+% This  software  is a copy  and  may not be current.  The latest  version is %
+% maintained by and may be obtained from the Mobility  and  Robotics  Sytstem %
+% Section (347) at the Jet  Propulsion  Laboratory.   Suggestions and patches %
+% are welcome and should be sent to the software's maintainer.                %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [voronoi_cells, voronoi_boundaries, voronoi_nodes] = get_voronoi_data(varargin)
+%GET_VORONOI_DATA computes information for a discrete Voronoi tesselation
+%from a given set of sources using wavefront propagation. 
+%Note: the current implemenation does not account for the Euclidian
+%distance between nodes. 
+%   [voronoi_cells, voronoi_boundaries, voronoi_nodes] = get_voronoi_data(sources, neighbor_set, *max_steps)
+%   *optional input
+%   
+%   Inputs: 
+%    - sources [N_SOURCES x 1]: The indicies for a set of source nodes on the small body 
+%    - neighbor_set [N_VERTICES x 1]: A cell containing the neighbors (i.e.
+%    connected verticies) for each vertex. 
+%       e.g. neighbor_set{i} will contain all verticies sharing a face with
+%       vertex i 
+% 
+%   Outputs: 
+%    - voronoi_cells [N_SOURCES x 1]: A cell containing the nodes in the
+%    coverage region for each Voronoi tesselation
+%       e.g. voronoi_cells{i} is a vector containing the indicies of all of 
+%       the points in the ith partition of the tesselation 
+%    - voronoi_boundaries [N_SOURCES x 1]: A cell containing the nodes
+%    composing the boundary of each of the repective partitions 
+%       e.g. voronoi_boundaries{i} is a vector containing the indicies of
+%       all of the nodes in the ith partition that have neighbors in a 
+%       different cell
+%    - voronoi_nodes [N_SOURCES x 1]: A cell containing the points in each 
+%    partition have neighboring nodes in at least two other cells 
 
 sources = varargin{1}; 
 neighbor_set = varargin{2};
@@ -21,12 +67,12 @@ for i = 1:n_sources
     voronoi_nodes{i} = [];
 end
 
-bordering_nodes = []; % candidate locations for the voronoi boundary
+bordering_nodes = []; % nodes "colliding" with other boundaries 
 
 %% Step 1: Calculate Voronoi Cells and Boundary
 
 for j = 1:max_steps % max_steps is search stop limit
-    for i=1:n_sources % Propagate source i
+    for i=1:n_sources 
         % Get candidate explored nodes
         [c_explored_vertices, c_boundary_set] = propagate_wavefront(voronoi_cells{i}, boundary_set{i}, neighbor_set, 1);
         
@@ -60,7 +106,7 @@ for j = 1:max_steps % max_steps is search stop limit
     end
 end
 
-% Create: Voronoi boundaries
+% Create Voronoi Boundaries
 for i=1:n_sources
     voronoi_boundaries{i} = intersect(voronoi_cells{i}, bordering_nodes); % voronoi_boundaries{i} = coverage points in cell i lying on edge of cell
 end
@@ -91,9 +137,6 @@ if n_sources>2
         end
     end
 end
-    
-
-
 
 end
 
