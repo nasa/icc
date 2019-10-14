b03b0fe28d3bf1a944fb1bb8f204e820d017838a
Federico Rossi (347N) <federico.rossi@jpl.nasa.gov>
2019-10-14 10:36:42 -0700

    Fix: SPICE downloader now works even with spaces in PATH

--- utilities/spice/download_SPICE_kernel.sh
+++ utilities/spice/download_SPICE_kernel.sh
@@ -12,18 +12,18 @@ echo "Downloading 433 EROS kernel"
 wget -q --show-progress ftp://ssd.jpl.nasa.gov/pub/ssd/SCRIPTS/smb_spk
 chmod 755 smb_spk
 ./smb_spk -b "Eros;" 2000-Jan-1 2025-JAN-1 $USER@jpl.nasa.gov a433.bsp
-mkdir -p $NAIF_PATH/naif/generic_kernels/spk/asteroids/
-mv a433.bsp $NAIF_PATH/naif/generic_kernels/spk/asteroids/
+mkdir -p "${NAIF_PATH}/naif/generic_kernels/spk/asteroids/"
+mv a433.bsp "${NAIF_PATH}/naif/generic_kernels/spk/asteroids/"
 
 echo "Downloading leap seconds kernel"
-mkdir -p naif/generic_kernels/lsk/
-wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls -P $NAIF_PATH/naif/generic_kernels/lsk/
+mkdir -p "${NAIF_PATH}/naif/generic_kernels/lsk/"
+wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls -P "${NAIF_PATH}/naif/generic_kernels/lsk/"
 
 echo "Downloading generic constants kernel"
-mkdir -p naif/generic_kernels/pck/
-wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc -P $NAIF_PATH/naif/generic_kernels/pck/
-wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/gm_de431.tpc -P $NAIF_PATH/naif/generic_kernels/pck/
+mkdir -p "${NAIF_PATH}/naif/generic_kernels/pck/"a
+wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc -P "${NAIF_PATH}/naif/generic_kernels/pck/"
+wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/gm_de431.tpc -P "${NAIF_PATH}/naif/generic_kernels/pck/"
 
 echo "Downloading planets kernel"
-mkdir -p naif/generic_kernels/spk/planets/
-wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de435.bsp -P $NAIF_PATH/naif/generic_kernels/spk/planets/
+mkdir -p "${NAIF_PATH}/naif/generic_kernels/spk/planets/"
+wget -q --show-progress ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de435.bsp -P "${NAIF_PATH}/naif/generic_kernels/spk/planets/"
