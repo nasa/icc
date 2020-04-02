98bdb30159bc8496da82175dbeace1a25261c665
Federico Rossi (347N) <federico.rossi@jpl.nasa.gov>
2020-04-01 17:03:37 -0700

    Add: plot observed and recorded points differently. Sometimes the observation optimizer decides to observe a point, but the communication optimizer decides not to record it. Now, we plot observed-and-recorded points in a color, and observed-only points in white. Resolves #51, resolves #53

--- visualization/render_observed_points_2d.m
+++ visualization/render_observed_points_2d.m
@@ -52,6 +52,7 @@ color_array = ['r', 'b', 'g', 'c', 'm']; %rand(3,Swarm.get_num_spacecraft());
 title_font_size = 30;
 standard_font_size = 25;
 font_name = 'Times New Roman';
+observed_not_recorded_color = [.85, .85 .85];
 
 if length(varargin) > 3
     for i = 3:1:length(varargin)
@@ -71,6 +72,9 @@ if length(varargin) > 3
         if strcmpi(varargin{i},'font_name')
             font_name = varargin{i+1};
         end
+        if strcmpi(varargin{i},'observed_not_recorded_color')
+            observed_not_recorded_color = varargin{i+1};
+        end
     end
 end
 
@@ -78,17 +82,27 @@ if timeLimitSpecified == false
     time_limits = [1, length(Swarm.sample_times)];
 end
 
-pos_points = AsteroidModel.BodyModel.shape.vertices ; % Convert to [km] for plotting
+% Location of the points
+pos_points = AsteroidModel.BodyModel.shape.vertices ; 
 n_spacecraft = Swarm.get_num_spacecraft();
 
+% Which spacecraft (if any) observes a point
 point_index = zeros(1,size(pos_points,1));
+point_rec_index = zeros(1,size(pos_points,1));
+
 for i_sc = 1:n_spacecraft
-    obs_points = Swarm.Observation.observed_points(i_sc,time_limits(1):time_limits(end));
-    obs_points(obs_points==0)= [];
-    point_index(obs_points) = i_sc;
+    for k=time_limits(1):time_limits(end)
+        obs_points = Swarm.Observation.observed_points(i_sc,time_limits(1):time_limits(end));
+        obs_points(obs_points==0)= [];
+        point_index(obs_points) = i_sc;
+        rec_points = Swarm.Observation.observed_points(i_sc,time_limits(1):time_limits(end)).*(full(Swarm.Communication.effective_source_flow(:,time_limits(1):time_limits(end)))>0);
+        rec_points(rec_points==0)= [];
+        point_rec_index(rec_points) = i_sc;
+    end
 end
 
 %% Plot
+
 
 cla()
 hold on
@@ -96,30 +110,30 @@ above_equator_index = transpose(logical(pos_points(:,3)>=0));
 not_observed_index = logical(point_index == 0);
 
 if strcmp(above_or_below, 'above')
-    above_equator_not_observed_index = (above_equator_index & not_observed_index);
-    
-    plot(pos_points(above_equator_not_observed_index,1),pos_points(above_equator_not_observed_index,2),...
-        '.k','MarkerFaceColor','none','MarkerSize',5)
-    
-    for i_sc = 1:1:n_spacecraft
-        observed_index = logical(point_index == i_sc);
-        above_equator_observed_index = (above_equator_index & observed_index);
-        plot(pos_points(above_equator_observed_index,1),pos_points(above_equator_observed_index,2),'o','MarkerFaceColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerEdgeColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerSize',5)
-    end
-    
+    my_side_index = above_equator_index;
 else
-    above_equator_not_observed_index = (~above_equator_index & not_observed_index);
-    
-    plot(pos_points(above_equator_not_observed_index,1),pos_points(above_equator_not_observed_index,2),...
-        '.k','MarkerFaceColor','none','MarkerSize',5)
-    
-    for i_sc = 1:1:n_spacecraft
-        observed_index = logical(point_index == i_sc);
-        above_equator_observed_index = (~above_equator_index & observed_index);
-        plot(pos_points(above_equator_observed_index,1),pos_points(above_equator_observed_index,2),'o','MarkerFaceColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerEdgeColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerSize',5)
-    end
-    
+    my_side_index = ~above_equator_index;
 end
+my_side_not_observed_index = (my_side_index & not_observed_index);
+% Plot the unobserved points as black dots.
+plot(pos_points(my_side_not_observed_index,1),pos_points(my_side_not_observed_index,2),...
+    '.k','MarkerFaceColor','none','MarkerSize',5)
+for i_sc = 1:1:n_spacecraft
+    % These are the points observed by this spacecraft
+    observed_index = logical(point_index == i_sc);
+    recorded_index = logical(point_rec_index == i_sc);
+    % And these are the points have observed AND are above the equator.
+    my_side_observed_index = (my_side_index & observed_index);
+    my_side_recorded_index = (my_side_index & recorded_index);
+
+    % Go plot the observed points
+    plot(pos_points(my_side_observed_index,1),pos_points(my_side_observed_index,2),'o','MarkerFaceColor',observed_not_recorded_color,'MarkerEdgeColor',observed_not_recorded_color,'MarkerSize',5)
+    % Go plot the observed AND recorded points
+%     plot(pos_points(my_side_recorded_index,1),pos_points(my_side_recorded_index,2),'o','MarkerFaceColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerEdgeColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerSize',5)
+    plot(pos_points(my_side_recorded_index,1),pos_points(my_side_recorded_index,2),'o','MarkerFaceColor','r','MarkerEdgeColor',color_array(:,mod(Swarm.Parameters.types{i_sc},size(color_array,2))+1)','MarkerSize',5)
+
+end
+
 title(strcat(upper(above_or_below(1)),above_or_below(2:end),' Equator'),'fontsize',title_font_size,'fontname',font_name)
 xlabel('X axis [km]','fontsize',standard_font_size, 'fontname',font_name)
 ylabel('Y axis [km]','fontsize',standard_font_size, 'fontname',font_name)
