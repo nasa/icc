d4de23bd8359b74ba7386fe3fbe18f90d55adfda
Federico Rossi (347N) <federico.rossi@jpl.nasa.gov>
2020-04-01 21:18:37 -0700

    Fix: speed up MILP formulation. The MILP formulation for the observable points optimizer was very slow (>1s). The bottleneck was a set of nested for loops scanning a _sparse_ matrix. We now access the matrix in a sparse way, resulting in a significant speedup.

--- observed_points_optimizer/swarm_points_optimizer.m
+++ observed_points_optimizer/swarm_points_optimizer.m
@@ -2,6 +2,12 @@ function [observed_points, priority] = swarm_points_optimizer(observable_points_
 %SWARM_POINTS_OPTIMIZER Optimizes the observed points of the agents in
 %sc_optimized
 
+% observable_points_map is a cell of size N (number of spacecraft)
+% Every entry in the cell is a sparse matrix of size Nv by K, where Nv is
+% the number of vertices and K is the number of timesteps.
+% observable_points_map{i}(v,t) is 1 iff point v is observable by agent i
+% at time t.
+
 %% Setup 
 tic
 
@@ -29,25 +35,42 @@ n_constraints = N*K + n_observed_vertices;
 % Create pointer vectors to keep track of the time, vertex, and agent
 % corresponding to the pth position in the decision variable
 p = 0; p_k=zeros(1,M); p_v=zeros(1,M); p_s=zeros(1,M); w=zeros(1,M);
-for k = 1:K
-    for i_v = 1:Nv
-        for i_sc = sc_optimized
-            if observable_points_map{i_sc}(i_v, k)==1 % if vertex i_v is observable at time k
-                p = p+1;
-                p_k(p) = k;
-                p_v(p) = i_v;
-                p_s(p) = i_sc;
-                w(p) = reward_map{i_sc}(i_v,k); % rewards corresponding to each potential observation
-            end
-        end
+for i_sc = sc_optimized
+    % observable_points_list is a vector of size n_nonzero_entries*3.
+    % the first column is the row location, in this case i_v.
+    % the second column is the column location, in this case k
+    % the third column is the value, in this case always 1
+    [i_v_vec, k_vec, ~] = find(observable_points_map{i_sc});
+    for observable_point_index = 1:length(i_v_vec)
+        i_v = i_v_vec(observable_point_index);
+        k = k_vec(observable_point_index);
+        p = p+1;
+        p_k(p) = k;
+        p_v(p) = i_v;
+        p_s(p) = i_sc;
+        w(p) = reward_map{i_sc}(i_v,k); % rewards corresponding to each potential observation
     end
+% Older code loops through all entries of the sparse matrix, not ideal
+%     for k = 1:K
+%         for i_v = 1:Nv
+%             if observable_points_map{i_sc}(i_v, k)==1 % if vertex i_v is observable at time k
+%                 p = p+1;
+%                 p_k(p) = k;
+%                 p_v(p) = i_v;
+%                 p_s(p) = i_sc;
+%                 w(p) = reward_map{i_sc}(i_v,k); % rewards corresponding to each potential observation
+%             end
+%         end
+%     end
 end
 
 % Store sets of indicies for observations by same spacecraft at same time
+num_constraint_entries = 0;
 sigma_ik = cell(N,K);
 for k = 1:K
     for i_sc = sc_optimized
         sigma_ik{i_sc,k} = intersect(find(p_k==k),find(p_s==i_sc));
+        num_constraint_entries = num_constraint_entries+length(sigma_ik{i_sc,k});
     end
 end
 
@@ -55,22 +78,46 @@ end
 sigma_v = cell(1,n_observed_vertices);
 for p = 1:n_observed_vertices
     sigma_v{p} = find(p_v==observable_vertices(p));
+    num_constraint_entries = num_constraint_entries+length(sigma_v{p});
 end
 
 %% Define Optimization Input Arrays
-A = zeros(n_constraints, M); 
+% Build A as a natively sparse matrix
+A = zeros(num_constraint_entries,3);
+
 i_constraint = 0;
-for k = 1:K
+i_entry = 0;
+for k=1:K
     for i_sc = sc_optimized
-        i_constraint = i_constraint+1;
-        A(i_constraint , sigma_ik{i_sc,k}) = 1;
+        i_constraint = i_constraint + 1;
+        for entry = sigma_ik{i_sc,k}
+            i_entry = i_entry +1;
+            A(i_entry, :) = [i_constraint, entry, 1];
+        end
     end
 end
 for p=1:n_observed_vertices
     i_constraint = i_constraint+1;
-    A(i_constraint, sigma_v{p}) = 1;
+    for entry= sigma_v{p}
+        i_entry = i_entry+1;
+        A(i_entry,:) = [i_constraint, entry, 1];
+    end
 end
-A = sparse(A);
+A = sparse(A(:,1), A(:,2), A(:,3), n_constraints, M);
+
+% A = zeros(n_constraints, M); 
+% i_constraint = 0;
+% for k = 1:K
+%     for i_sc = sc_optimized
+%         i_constraint = i_constraint+1;
+%         A(i_constraint , sigma_ik{i_sc,k}) = 1;
+%     end
+% end
+% for p=1:n_observed_vertices
+%     i_constraint = i_constraint+1;
+%     A(i_constraint, sigma_v{p}) = 1;
+% end
+% A = sparse(A);
 b = ones(n_constraints, 1);
 
 % Set bounds
