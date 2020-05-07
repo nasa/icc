4c8ae4c007cbca149e0e814d93609bee4251b391
Saptarshi Bandyopadhyay <Saptarshi.Bandyopadhyay@jpl.nasa.gov>
2020-05-07 07:05:56 +0530

    Updated_run_all_demo_files

--- examples/Run_all_demo_files.m
+++ examples/Run_all_demo_files.m
@@ -1,11 +1,11 @@
 % Run all demo files
 clear all; close all; clc;
 
+% version 1 codes
+
 ICC_Simulation;
 
 Monte_Carlo_Coverage_Optimization_Demo;
-
-Observation_and_Communication_Optimizer_Demo;
 
 Communication_Optimization_Demo;
 
@@ -24,3 +24,10 @@ SBDT_Demo_Eros;
 Plot_Orbits;
 
 
+% version 2 codes
+
+Observation_and_Communication_Optimizer_Demo;
+
+Integrated_ICC_Optimization_Demo;
+
+test_observation_and_communication_optimizer;
